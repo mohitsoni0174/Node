69ec14d3c584a533b74ccb9a089a17eec3aa4952
mohitsoni0174 <mohitsoni1704@gmail.com>
2026-09-02 21:15:10 +0530

    database created local

--- notesapp/src/app.js
+++ notesapp/src/app.js
@@ -2,8 +2,30 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Welcome to the Notes App!");
+});
+
+app.post("/create", (req, res) => {
+  try {
+    let { title, description } = req.body;
+
+    let newNote = new notesModel.create({
+      title,
+      description,
+    });
+    return res.status(201).json({
+      message: "Note created successfully",
+      note: newNote,
+    });
+  } catch (error) {
+    console.log("error in creating note", error);
+    res.status(500).json({
+      message: "Error creating note",
+    });
+  }
 });
 
 module.exports = app;
